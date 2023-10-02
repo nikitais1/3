fd03a4b65b12d95341a7b8a58214b41138495c01
Nikita <nikita.isaev2001@mail.ru>
2023-10-02 19:12:23 +0300

    Changed link in main function

--- src/main.py
+++ src/main.py
@@ -2,8 +2,10 @@ import json
 
 from src.utils import filter_and_sorting, prepare_user_message
 
-with open('operations.json', 'r', encoding='utf-8') as file:
+with open('../data/operations.json', 'r', encoding='utf-8') as file:
     data = json.load(file)
 items = filter_and_sorting(data)
 for i in range(5):
     print(prepare_user_message(items[i]))
+
+
